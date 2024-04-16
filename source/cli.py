1ed832b32856ae71eec74ac8008e8cd0b263101e
shirrashko <srashkovits@lightricks.com>
2024-04-16 15:29:03 +0300

    Add adjustments

--- source/cli.py
+++ source/cli.py
@@ -1,18 +1,16 @@
 import argparse
 from image_processor import ImageProcessor
 
-
 class CommandLineInterface:
     def __init__(self):
         self.parser = self.setup_parser()
-        self.processed_image = None
 
     @staticmethod
     def setup_parser():
         parser = argparse.ArgumentParser(description="Advanced Image Editing CLI Tool")
         parser.add_argument("--image", required=True, help="Path to the image file")
         parser.add_argument("--filter", help="Apply a filter")
-        parser.add_argument("--adjust", action='append', nargs=2, help="Adjust image properties")
+        parser.add_argument("--adjust", action='append', nargs=2, metavar=('ADJUSTMENT', 'VALUE'), help="Adjust image properties e.g., --adjust brightness 1.5")
         parser.add_argument("--save", help="Path to save the edited image")
         parser.add_argument("--display", action='store_true', help="Display the edited image")
         return parser
@@ -24,9 +22,9 @@ class CommandLineInterface:
         if args.filter:
             processor.apply_filter(args.filter)
 
-        # if args.adjust:
-        #     for adj, value in args.adjust:
-        #         processor.adjust_image(adj, int(value))
+        if args.adjust:
+            for adjustment, value in args.adjust:
+                processor.adjust_image(adjustment, float(value))
 
         if args.save:
             processor.save_image(args.save)
--- source/image_processor.py
+++ source/image_processor.py
@@ -1,5 +1,6 @@
 from source.custom_image import CustomImage
 from filters import BlurFilter, EdgeDetectionFilter, SharpenFilter
+from PIL import ImageEnhance
 
 
 class ImageProcessor:
@@ -18,6 +19,18 @@ class ImageProcessor:
             self.processed_image = filter_instance.apply(self.custom_image)
         else:
             raise ValueError(f"Filter '{filter_name}' not supported.")
+
+    def adjust_image(self, adjustment, value):
+        if adjustment == "brightness":
+            enhancer = ImageEnhance.Brightness(self.custom_image.image)
+        elif adjustment == "contrast":
+            enhancer = ImageEnhance.Contrast(self.custom_image.image)
+        elif adjustment == "saturation":
+            enhancer = ImageEnhance.Color(self.custom_image.image)
+        else:
+            return  # If adjustment type is unknown, do nothing
+
+        self.processed_image = enhancer.enhance(value)
 
     def save_image(self, path):
         self.processed_image.save(path)
